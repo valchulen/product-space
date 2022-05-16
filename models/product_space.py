d6afd36cc01d0a2d263b56f16dbcdeab299dd73e
Tobias Carreira Munich <tobiascarreira@gmail.com>
2022-05-16 15:30:28 -0300

    Fix cuenta phi y bug usando numpy para calcular phi

--- models/product_space.py
+++ models/product_space.py
@@ -36,11 +36,19 @@ class ProductSpace(CoupledDEVS):
         self._calculate_phi_matrix(get_run_parameters().X)
 
     def _calculate_phi_matrix(self, X):
-        phi = X @ X.T / (X.shape[1] ** 2)
-        S = np.sum(X, axis=1)
-        M = np.ones_like(S) @ S.T
-        M = (M * (M <= M.T)) + (M.T * (M > M.T))
-        self.state["phi_matrix"] = self.phi_matrix = phi * M
+        # phi[i,j] = sumatoria_c (Xci * Xcj)
+        # cantidad de paises que exportan tanto i como j
+        phi = X @ X.T
+        # cantidad de paises que exportan cada producto
+        S = phi.diagonal()
+        # matriz que en cada fila tiene a S
+        M = np.reshape(np.ones_like(S), (-1, 1)) @ np.reshape(S, (1, -1))
+        # M[i,j] = max(sumatoria_c Xci, sumatoria_c Xcj)
+        # dividir por el maximo me va a dar el minimo
+        M = (M * (M >= M.T)) + (M.T * (M < M.T))
+        # cuenta final,out y where hacen que la probabilidad sea 0 cuando se intenta dividir por 0
+        # esto pasa cuando ninguno de los dos productos se exportan en ningun pais
+        self.state["phi_matrix"] = self.phi_matrix = np.divide(phi, M, out=np.zeros_like(phi, dtype=np.float64), where=(M!=0))
 
     def getContextInformation(self, property, *args, **kwargs):
         if property == ProductSpaceProps.PHI_MATRIX:
